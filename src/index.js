1d7877fd7f68fe69a48101a32f7e8e4090ea642a
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2020-12-12 23:48:59 +0200

    hw#10

--- src/index.js
+++ src/index.js
@@ -1,37 +1,45 @@
 import css from './styles.css';
 import menu from './menu.json';
+import menuTemplate from './templates/menu-items.hbs';
 
 const refs = {
   switchToggle: document.querySelector('#theme-switch-toggle'),
   body: document.querySelector('body'),
+  menu: document.querySelector('.js-menu'),
 };
 
 const Theme = {
-  LIGHT: 'theme-light',
-  DARK: 'theme-dark',
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
 };
 
-const LIGHT = JSON.stringify(Theme.LIGHT);
-const DARK = JSON.stringify(Theme.DARK);
-
-// refs.body.classList.add('light-theme');
 refs.switchToggle.addEventListener('change', themeSwitch);
+
+savedDarkTheme();
 
 function themeSwitch(event) {
   const inputChecked = event.currentTarget.checked;
   if (inputChecked === false) {
     refs.body.classList.add('light-theme');
     refs.body.classList.remove('dark-theme');
-    localStorage.setItem('light', LIGHT);
-    localStorage.removeItem('dark', DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
   }
   if (inputChecked === true) {
     refs.body.classList.remove('light-theme');
     refs.body.classList.add('dark-theme');
-    localStorage.setItem('dark', DARK);
-    localStorage.removeItem('light', LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
   }
-  console.dir(event.currentTarget.checked);
 }
 
-console.log(Theme);
+function savedDarkTheme() {
+  const themeSaved = localStorage.getItem('theme');
+  if (themeSaved === Theme.DARK) {
+    refs.body.classList.add('dark-theme');
+    refs.switchToggle.checked = true;
+  }
+}
+
+const markup = menuTemplate(menu);
+
+refs.menu.insertAdjacentHTML('beforeend', markup);
+console.log(refs.menu);
